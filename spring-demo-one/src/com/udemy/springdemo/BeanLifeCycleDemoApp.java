534366817a9dc3afaf02fcfdf0610ccb3df1a22b
Anthony Price <antprice07@gmail.com>
2021-02-16 13:13:59 -0500

    finished first 6 sections

---
+++ spring-demo-one/src/com/udemy/springdemo/BeanLifeCycleDemoApp.java
@@ -0,0 +1,21 @@
+package com.udemy.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BeanLifeCycleDemoApp
+{
+
+	public static void main(String[] args)
+	{
+		// load the config
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("beanLifeCycle-applicationContext.xml");
+		// retrieve bean from container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+
+		System.out.println(theCoach.getDailyWorkout());
+
+		// close context
+		context.close();
+	}
+
+}
